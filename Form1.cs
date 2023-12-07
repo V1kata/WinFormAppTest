a0d63ff5d161cf54b973e1edb15ee9ded6065445
V1kata <116576258+V1kata@users.noreply.github.com>
2023-12-07 18:12:15 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,81 +1,100 @@
-
 namespace winforms;
 
-partial class Form1
+public partial class Form1 : Form
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    List<User> users = new List<User>();
+    public Form1()
     {
-        if (disposing && (components != null))
-        {
-            components.Dispose();
+        InitializeComponent();
+    }
+
+    public void AddUser(object sender, EventArgs e) {
+        if (CheckIfThereIsData()) {
+            MessageBox.Show("Please fill all fields");
+            return;
         }
-        base.Dispose(disposing);
+
+        for (int i = 0; i < users.Count; i++) {
+            if (users[i].Username == this.txtBox1.Text) {
+                MessageBox.Show($"User {this.txtBox1.Text} is already regiters");
+                ClearFields();
+                return;
+            }
+        }
+
+        User user = new User(this.txtBox1.Text, this.txtBox2.Text);
+        users.Add(user);
+
+        ClearFields();
     }
 
-    #region Windows Form Designer generated code
+    public void ReadUser(object sender, EventArgs e) {
+        string userData = "";
 
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(400, 450);
-        this.Text = "Form1";
+        foreach (var item in users)
+        {
+            userData += item.SeeData();
+        }
 
-        this.addBtn = new Button();
-        this.addBtn.Text = "Add user";
-        this.addBtn.Location = new Point(0, 225);
-        this.addBtn.Click += AddUser;
-        this.Controls.Add(addBtn);
-
-        this.updateBtn = new Button();
-        this.updateBtn.Text = "Update user";
-        this.updateBtn.Location = new Point(200, 225);
-        this.updateBtn.Click += UpdateUser;
-        this.Controls.Add(updateBtn);
-
-        this.readBtn = new Button();
-        this.readBtn.Text = "Read all users";
-        this.readBtn.Location = new Point(100, 225);
-        this.readBtn.Click += ReadUser;
-        this.Controls.Add(readBtn);
-
-        this.deleteBtn = new Button();
-        this.deleteBtn.Text = "Delete user";
-        this.deleteBtn.Click += DeleteUser;
-        this.deleteBtn.Location = new Point(300, 225);
-        this.Controls.Add(deleteBtn);
-
-        this.txtBox1 = new TextBox();
-        this.txtBox1.PlaceholderText = "Username";
-        this.txtBox1.Location = new Point(50, 50);
-        this.Controls.Add(txtBox1);
-
-        this.txtBox2 = new TextBox();
-        this.txtBox2.PasswordChar = '*';
-        this.txtBox2.PlaceholderText = "Password";
-        this.txtBox2.Location = new Point(250, 50);
-        this.Controls.Add(txtBox2);
+        MessageBox.Show(userData);
+        ClearFields();
     }
 
-    #endregion
+    public void UpdateUser(object sender, EventArgs e) {
+        if (CheckIfThereIsData()) {
+            MessageBox.Show("Please fill all fields");
+            return;
+        }
 
-    public Button addBtn;
-    public Button updateBtn;
-    public Button readBtn;
-    public Button deleteBtn;
-    public TextBox txtBox1;
-    public TextBox txtBox2;
+        User currentUser;
+        string username = this.txtBox1.Text;
+
+        for (int i = 0; i < users.Count; i++) {
+            if (users[i].Username == username) {
+                currentUser = users[i];
+                currentUser.Password = this.txtBox2.Text;
+                users[i] = currentUser;
+                MessageBox.Show($"User with {username} was updated");
+                break;
+            } else {
+                MessageBox.Show("User doesn't exist");
+            }
+        }
+
+        ClearFields();
+    }
+
+    public void DeleteUser(object sender, EventArgs e) {
+        if (this.txtBox1.Text == "") {
+            MessageBox.Show("Please fill all fields");
+            return;
+        }
+
+        string username = this.txtBox1.Text;
+
+        for (int i = 0; i < users.Count; i++) {
+            if (users[i].Username == username) {
+                users.RemoveAt(i);
+                MessageBox.Show($"User with {username} was deleted");
+                break;
+            } else {
+                MessageBox.Show("User doesn't exist");
+            }
+        }
+
+        ClearFields();
+    }
+
+    private void ClearFields() {
+        this.txtBox1.Text = "";
+        this.txtBox2.Text = "";
+    }
+
+    private bool CheckIfThereIsData() {
+        if (this.txtBox1.Text == "" || this.txtBox2.Text == "") {
+            return true;
+        }
+
+        return false;
+    }
 }
